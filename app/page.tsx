5b962b002b12d4605c262f3cc8179ba315713e8d
SanseiDororo <philoscopes@gmail.com>
2023-10-17 10:27:48 +0200

    Displaying Bugs on the dasboard added

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,53 @@
 'use client'
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import { Card, Flex, Box, Text } from '@radix-ui/themes'
+
+interface Issue {
+  id: number
+  title: string
+  description: string
+}
 
 export default function Home() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<Issue[] | null>(null)
 
   useEffect(() => {
     const fetchIssues = async () => {
       try {
-        // Make a GET request to the API route
         const response = await axios.get('/api/issues')
-
-        // Handle the response data here and set it in the state
         setData(response.data)
         console.log('Data from the API:', response.data)
       } catch (error) {
-        // Handle any errors here
         console.error('Error fetching data from the API:', error)
       }
     }
-
-    // Call the fetchIssues function immediately
     fetchIssues()
   }, [])
 
   return (
     <div>
       {data ? (
-        <div>
+        <div className="flex flex-row gap-6 ml-6">
           {data.map((issue) => (
             <div key={issue.id}>
-              <p>Title: {issue.title}</p>
-              <p>Description: {issue.description}</p>
+              <Card style={{ maxWidth: 240 }}>
+                <Flex gap="3" align="center">
+                  <Box>
+                    <Text as="div" size="4" weight="bold" color="red" mb="4">
+                      {issue.title}
+                    </Text>
+                    <Text as="div" size="3" color="gray">
+                      {issue.description}
+                    </Text>
+                  </Box>
+                </Flex>
+              </Card>
             </div>
           ))}
         </div>
       ) : (
-        <p>Loading data...</p>
+        <p className="ml-11">Loading data...</p>
       )}
     </div>
   )
